ae31851cf7abee6357a4ff9497adc00ff1414bd4
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2024-12-20 19:23:49 +0530

    add: sprite with animation

---
+++ app/components/sprite.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Sprite = () => {
+  return (
+    <div className="sprite">
+      <div className="eye left"></div>
+      <div className="eye right"></div>
+      <div className="star one"></div>
+      <div className="star two yellow"></div>
+      <div className="star three green"></div>
+      <div className="star four"></div>
+    </div>
+  );
+};
+
+export default Sprite;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import Hero from "./components/hero";
+import Navbar from "./components/navbar";
+
 export default function Home() {
-  return <div className="font-bold">Hello World</div>;
+  return (
+    <div className="font-bold">
+      <section>
+        <Navbar />
+        <Hero />
+      </section>
+    </div>
+  );
 }
